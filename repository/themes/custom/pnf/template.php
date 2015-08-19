427385891f5a3bcb0fa22fdae82849437bdc029a
Adrien Revel <adrien@abrege.net>
2015-08-19 14:40:53 +0200

    Update custom theme pnf to use custom less files by domain

--- repository/themes/custom/pnf/template.php
+++ repository/themes/custom/pnf/template.php
@@ -4,3 +4,45 @@
  * @file
  * template.php
  */
+
+/**
+ * Loads compiled stylesheet depending on site URL.
+ */
+
+$parts = array_reverse(explode('.', $_SERVER['SERVER_NAME']));
+
+switch($parts[1]) {
+	case "calanques-parcnational":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
+	break;
+	case "cevennes-parcnational":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
+	break;
+	case "ecrins-parcnational":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
+	break;
+	case "guadeloupe-parcnational":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
+	break;
+	case "mercantour":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
+	break;
+	case "parc-amazonien-guyane":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
+	break;
+	case "parc-pyrenees":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
+	break;
+	case "parcsnationaux":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
+	break;	
+	case "portcrosparcnational":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
+	break;
+	case "reunion-parcnational":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
+	break;
+	case "vanoise-parcnational":
+		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
+	break;	
+}
