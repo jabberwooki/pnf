11361c8eb4bf68426ced569c150f00c40d5d4ba3
sographik <sographik@gmail.com>
2015-09-24 17:20:36 +0200

    A la une css + js

---
+++ repository/themes/custom/pnf/js/pnf.js
@@ -0,0 +1,11 @@
+
+
+(function($) {
+  $(document).ready(function() {
+			
+	$('.view-mode-teaser_home').matchHeight('options');
+	$('.view-mode-teaser_home .field-name-body p').matchHeight('options');
+	$('.view-mode-teaser_home .field-name-title h3').matchHeight('options');
+
+ });
+})(jQuery);
