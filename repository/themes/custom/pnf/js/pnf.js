2510d1365ad5e21928e9de0897a5f778ee3a6e21
Adrien Revel <adrien@abrege.net>
2015-11-20 17:42:56 +0100

    Theme updated: add styles and jQuery for img resume overlay.

--- repository/themes/custom/pnf/js/pnf.js
+++ repository/themes/custom/pnf/js/pnf.js
@@ -5,27 +5,10 @@
   $('#block-menu-menu-menu-park, #block-menu-menu-national-parks').addClass('dropdown');
   $('#block-menu-menu-menu-park ul li ul, #block-menu-menu-national-parks ul li ul').addClass('dropdown-menu');
 
-
-// Match Height for teasers of news and agenda
-	$('.view-news .view-mode-teaser_home, .view-news .view-mode-teaser').matchHeight('options');
-	$('.view-news .view-mode-teaser_home .field-name-body p, .view-news .view-mode-teaser .field-name-body p').matchHeight('options');
-	$('.view-news .view-mode-teaser_home .field-name-title h3, .view-news .view-mode-teaser .field-name-title h3').matchHeight('options');
-
-  $('.page-agenda .view-mode-teaser').matchHeight('options');
-  $('.page-agenda .view-mode-teaser h3').matchHeight('options');
-  
-
 // Position the mega menu
-  positionMegaMenuDropDown();
-	$( window ).resize( positionMegaMenuDropDown );
+    positionMegaMenuDropDown();
+    $( window ).resize( positionMegaMenuDropDown );
 
-// DEF Function positionMegaMenuDropDown
-    function positionMegaMenuDropDown() {
-    	$('.mega-dropdown-menu').width($(window).width());
-   		var offset = $('ul.tb-megamenu-nav').offset();
-    	//console.log(offset.left);
-    	$('.mega-dropdown-menu').css({ "margin-left": - offset.left});
-    }
 
 // Remove data-toggle attribute from first level item link to make it clickable
 // in current park and all parks menus.
@@ -34,5 +17,65 @@
     $('#block-menu-menu-menu-park ul li.first a').removeAttr( "data-toggle" );
     $('#block-menu-menu-national-parks ul li.first a').removeAttr( "data-toggle" );
 
+
+
+
+
+// Match Height for teasers of news and agenda
+  $('.view-news .view-mode-teaser_home, .view-news .view-mode-teaser').matchHeight('options');
+  //$('.view-news .view-mode-teaser_home .field-name-body p, .view-news .view-mode-teaser .field-name-body p').matchHeight('options');
+  $('.view-news .view-mode-teaser_home .field-name-title h3, .view-news .view-mode-teaser .field-name-title h3').matchHeight('options');
+  $('.page-agenda .view-mode-teaser').matchHeight('options');
+  $('.page-agenda .view-mode-teaser h3').matchHeight('options');
+
+/* Resumes image overlay */
+  setParagraphHeight();
+
+  $( window ).resize( setParagraphHeight );
+
+  $('.group-resume p').hide();
+  
+  $('.group-resume').mouseenter(function() {
+    $('p', this).slideDown('fast');
+  });
+
+  $('.group-resume').mouseleave(function() {
+    $('p', this).slideUp('slow');
+  });
+
+/* Resumes p link to node */
+
+$('.group-resume').click(function(event) {
+  var link = $('a',this).attr('href');
+  //console.log(link);
+  window.location = link;
+});
+
+
+
+
+
+// DEF Function setParagraphHeight
+    function setParagraphHeight() {
+    var imgHeight = $('.group-resume img').height() - 20;
+    //console.log(imgHeight);
+    $('.group-resume p').height(imgHeight);
+
+    }
+
+
+// DEF Function positionMegaMenuDropDown
+    function positionMegaMenuDropDown() {
+      $('.mega-dropdown-menu').width($(window).width());
+      var offset = $('ul.tb-megamenu-nav').offset();
+      //console.log(offset.left);
+      $('.mega-dropdown-menu').css({ "margin-left": - offset.left});
+    }
+
  });
+
+ 
+
+
+
 })(jQuery);
