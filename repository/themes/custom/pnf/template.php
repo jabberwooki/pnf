f67944651ad9ef0db17e1ef32da514d132c53948
Christophe Espiau <christophe.espiau@korora.fr>
2015-12-11 20:28:20 +0100

    Custom PNF theme up: Reorganisation of code uploading css file depending on server name.

--- repository/themes/custom/pnf/template.php
+++ repository/themes/custom/pnf/template.php
@@ -16,158 +16,149 @@ function pnf_preprocess_page(&$variables) {
 }
 
 /**
- * Loads compiled stylesheet depending on site URL, OK for dev, staging and production sites.
+ * Loads compiled stylesheet depending on site URL, OK for dev, staging, pre-production and production sites.
  */
 
-$parts = array_reverse(explode('.', $_SERVER['SERVER_NAME']));
+$parts = explode('.', $_SERVER['SERVER_NAME']);
 
-if ($parts[0] != 'xyz') {
-	switch($parts[1]) {
-		case 'calanques-parcnational':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
-		break;
-		case 'cevennes-parcnational':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
-		break;
-		case 'ecrins-parcnational':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
-		break;
-		case 'guadeloupe-parcnational':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
-		break;
-		case 'mercantour':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
-		break;
-		case 'parc-amazonien-guyane':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
-		break;
-		case 'parc-pyrenees':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
-		break;
-		case 'parcsnationaux':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
-		break;	
-		case 'portcrosparcnational':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
-		break;
-		case 'reunion-parcnational':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
-		break;
-		case 'vanoise-parcnational':
-			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
-		break;	
-	}
-} else {
-	switch($parts[2]) {
-	  // Calanque
-	  case 'pnca':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
-		break;
-	  case 'preprod-pncal':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
-		break;
-	  case 'prod-pncal':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
-		break;
-	  // Cévennes
-	  case 'pnce':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
-		break;
-	  case 'preprod-pnc':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
-		break;
-	  case 'prod-pnc':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
-		break;
-	  // Ecrins
-	  case 'pnec':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
-		break;
-	  case 'preprod-pne':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
-		break;
-	  case 'prod-pne':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
-		break;
-	  // Guadeloupe
-	  case 'pngd':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
-		break;
-	  case 'preprod-png':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
-		break;
-	  case 'prod-png':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
-		break;
-	  // Mercantour
-	  case 'pnme':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
-		break;
-	  case 'preprod-pnm':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
-		break;
-	  case 'prod-pnm':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
-		break;
-	  // Amazonien de Guyane
-	  case 'pnag':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
-		break;
-	  case 'preprod-pag':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
-		break;
-	  case 'prod-pag':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
-		break;
-	  // Pyrénées
-	  case 'pnpy':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
-		break;
-	  case 'preprod-pnp':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
-		break;
-	  case 'prod-pnp':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
-		break;
-	  // Parcs nationaux de France
-	  case 'pnf':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
-		break;
-	  case 'preprod-pnf':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
-		break;
-	  case 'prod-pnf':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
-		break;
-	  // Port-Cros
-	  case 'pnpc':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
-		break;
-	  case 'preprod-pnpc':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
-		break;
-	  case 'prod-pnpc':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
-		break;
-	  // Réunion
-	  case 'pnre':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
-		break;
-	  case 'preprod-pnrun':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
-		break;
-	  case 'prod-pnrun':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
-		break;
-	  // Vanoise
-	  case 'pnva':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
-		break;
-	  case 'preprod-pnv':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
-		break;
-	  case 'prod-pnv':
-		drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
-		break;
-	}
+// dev and live instances :
+// - www.<park-name>.dev
+// - www.<park-name>.fr
+if ($parts[0] == 'www') {
+  switch ($parts[1]) {
+	case 'calanques-parcnational':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
+	  break;
+	case 'cevennes-parcnational':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
+	  break;
+	case 'ecrins-parcnational':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
+	  break;
+	case 'guadeloupe-parcnational':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
+	  break;
+	case 'mercantour':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
+	  break;
+	case 'parc-amazonien-guyane':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
+	  break;
+	case 'parc-pyrenees':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
+	  break;
+	case 'parcsnationaux':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
+	  break;
+	case 'portcrosparcnational':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
+	  break;
+	case 'reunion-parcnational':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
+	  break;
+	case 'vanoise-parcnational':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
+	  break;
+  }
+}
+// AR staging instance :
+// - <acronym>.pnfdev.xyz
+elseif ($parts[2] == 'xyz') {
+  switch ($parts[0]) {
+	// Calanque
+	case 'pnca':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
+	  break;
+	// Cévennes
+	case 'pnce':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
+	  break;
+	// Ecrins
+	case 'pnec':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
+	  break;
+	// Guadeloupe
+	case 'pngd':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
+	  break;
+	// Mercantour
+	case 'pnme':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
+	  break;
+	// Amazonien de Guyane
+	case 'pnag':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
+	  break;
+	// Pyrénées
+	case 'pnpy':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
+	  break;
+	// Parcs nationaux de France
+	case 'pnf':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
+	  break;
+	// Port-Cros
+	case 'pnpc':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
+	  break;
+	// Réunion
+	case 'pnre':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
+	  break;
+	// Vanoise
+	case 'pnva':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
+	  break;
+  }
+}
+// preprod and prod instances :
+// - preprod-<acronym>.<park-name>.fr
+// - prod-<acronym>.<park-name>.fr
+else {
+  switch(substr($parts[0], strpos($parts[0], '-') +1)) {
+	// Calanque
+	case 'pncal':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
+	  break;
+	// Cévennes
+	case 'pnc':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
+	  break;
+	// Ecrins
+	case 'pne':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
+	  break;
+	// Guadeloupe
+	case 'png':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
+	  break;
+	// Mercantour
+	case 'pnm':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
+	  break;
+	// Amazonien de Guyane
+	case 'pag':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
+	  break;
+	// Pyrénées
+	case 'pnp':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
+	  break;
+	// Parcs nationaux de France
+	case 'pnf':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
+	  break;
+	// Port-Cros
+	case 'pnpc':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
+	  break;
+	// Réunion
+	case 'pnrun':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
+	  break;
+	// Vanoise
+	case 'pnv':
+	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
+	  break;
+  }
 }
