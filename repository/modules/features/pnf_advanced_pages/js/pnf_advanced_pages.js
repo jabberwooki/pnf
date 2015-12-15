a4f7d210003e4797537c0f34d83a78e09817afec
Christophe Espiau <christophe.espiau@korora.fr>
2015-12-15 20:10:51 +0100

    pnf_advanced_pages up: Color selection mechanism for colored-bg-text bundle fixed.

--- repository/modules/features/pnf_advanced_pages/js/pnf_advanced_pages.js
+++ repository/modules/features/pnf_advanced_pages/js/pnf_advanced_pages.js
@@ -5,13 +5,13 @@
 (function ($) {
   Drupal.behaviors.advanced_pages = {
     attach: function (context, settings) {
-      // Make clickable the available colors displayed below the field field_bg_color.
-      $("span.pnf-bg-color").each(function () {
+      $("span.pnf-bg-color").each(function() {
         $(this).css('cursor', 'pointer');
-        $(this).click(function () {
-          color = $(this).text();
-          $("#edit-field-page-content-und-0-field-bg-color-und-0-value").val(color);
-        });
+      });
+      $("span.pnf-bg-color").click(function() {
+        parent_container_id = $(this).parent().parent().parent().attr('id');
+        color_nb = $(this).text();
+        $("#" + parent_container_id + " input").val(color_nb);
       });
 
       // jQuery actions depending on the various Paragraphs bundles displayed.
@@ -19,7 +19,7 @@
         // Text background coloration for bundle colored_bg_text.
         if (settings.pnf_advanced_pages_view.bg_color) {
           bg_color = settings.pnf_advanced_pages_view.bg_color;
-          $('div.paragraphs-item-colored-bg-text div.field-name-field-texte').attr('style', 'background-color: #' + bg_color);
+          //$('div.paragraphs-item-colored-bg-text div.field-name-field-texte').attr('style', 'background-color: #' + bg_color);
         }
         //
         if (settings.pnf_advanced_pages_view.images_group_item) {
