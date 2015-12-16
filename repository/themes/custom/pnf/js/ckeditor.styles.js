b1d91756628d1eb61ce1b3dfd71a4d1f9480926c
sographik <sographik@gmail.com>
2015-12-16 14:56:03 +0100

    modifs ckeditor js et css paragraph

--- repository/themes/custom/pnf/js/ckeditor.styles.js
+++ repository/themes/custom/pnf/js/ckeditor.styles.js
@@ -24,39 +24,7 @@ if(typeof(CKEDITOR) !== 'undefined') {
             { name : 'Titre 3 - vert'       , element : 'h3', styles : { 'color' : '#c4d600' } },
             { name : 'Titre 3 - marron'     , element : 'h3', styles : { 'color' : '#b86125' } },
 
-            /* Inline Styles */
 
-            // These are core styles available as toolbar buttons. You may opt enabling
-            // some of them in the "Styles" drop-down list, removing them from the toolbar.
-           
-            { name : 'Strong'			, element : 'strong', overrides : 'b' },
-            { name : 'Emphasis'			, element : 'em'	, overrides : 'i' },
-            { name : 'Underline'		, element : 'u' },
-            { name : 'Strikethrough'	, element : 'strike' },
-            { name : 'Subscript'		, element : 'sub' },
-            { name : 'Superscript'		, element : 'sup' },
-           
-
-            { name : 'Marker: Yellow'	, element : 'span', styles : { 'background-color' : 'Yellow' } },
-            { name : 'Marker: Green'	, element : 'span', styles : { 'background-color' : 'Lime' } },
-
-            { name : 'Big'				, element : 'big' },
-            { name : 'Small'			, element : 'small' },
-            { name : 'Typewriter'		, element : 'tt' },
-
-            { name : 'Computer Code'	, element : 'code' },
-            { name : 'Keyboard Phrase'	, element : 'kbd' },
-            { name : 'Sample Text'		, element : 'samp' },
-            { name : 'Variable'			, element : 'var' },
-
-            { name : 'Deleted Text'		, element : 'del' },
-            { name : 'Inserted Text'	, element : 'ins' },
-
-            { name : 'Cited Work'		, element : 'cite' },
-            { name : 'Inline Quotation'	, element : 'q' },
-
-            { name : 'Language: RTL'	, element : 'span', attributes : { 'dir' : 'rtl' } },
-            { name : 'Language: LTR'	, element : 'span', attributes : { 'dir' : 'ltr' } },
 
             /* Object Styles */
 
