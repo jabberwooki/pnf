753fb4b072feb3cdcdeda50cd6d200807d33c96f
Christophe Espiau <christophe.espiau@korora.fr>
2016-02-03 16:18:45 +0100

    PNF custom theme up: meta tag 'google-site-verification' added dynamically depending on domain name in template.php.

--- repository/themes/custom/pnf/template.php
+++ repository/themes/custom/pnf/template.php
@@ -5,10 +5,6 @@
  * template.php
  */
 
-
-
-
-
 /*respônsive image*/
 function pnf_preprocess_image(&$vars) {
         $vars['attributes']['class'][] = 'img-responsive'; // http://getbootstrap.com/css/#overview-responsive-images
@@ -30,40 +26,49 @@ $parts = explode('.', $_SERVER['SERVER_NAME']);
 // - www.<park-name>.dev
 // - www.<park-name>.fr
 if ($parts[0] == 'www') {
+	global $google_validation_key;
+	$google_validation_key = 'none';
+
+	global $is_live_domain;
+	$is_live_domain = ($parts[2] == 'fr' || $parts[2] == 'eu');
+
   switch ($parts[1]) {
-	case 'calanques-parcnational':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
-	  break;
-	case 'cevennes-parcnational':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
-	  break;
-	case 'ecrins-parcnational':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
-	  break;
-	case 'guadeloupe-parcnational':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
-	  break;
-	case 'mercantour':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
-	  break;
-	case 'parc-amazonien-guyane':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
-	  break;
-	case 'parc-pyrenees':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
-	  break;
-	case 'parcsnationaux':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
-	  break;
-	case 'portcrosparcnational':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
-	  break;
-	case 'reunion-parcnational':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
-	  break;
-	case 'vanoise-parcnational':
-	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
-	  break;
+		case 'calanques-parcnational':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnca_style.css.less');
+			break;
+		case 'cevennes-parcnational':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnce_style.css.less');
+			if ($is_live_domain) $google_validation_key = '8hrvr44vC-SC_mz27Ur2CZwvCyoqp5eho2U_IQnWQTM';
+			break;
+		case 'ecrins-parcnational':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnec_style.css.less');
+			break;
+		case 'guadeloupe-parcnational':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pngd_style.css.less');
+			break;
+		case 'mercantour':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnme_style.css.less');
+			break;
+		case 'parc-amazonien-guyane':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnag_style.css.less');
+			break;
+		case 'parc-pyrenees':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpy_style.css.less');
+			break;
+		case 'parcsnationaux':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnf_style.css.less');
+			break;
+		case 'portcrosparcnational':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnpc_style.css.less');
+			break;
+		case 'reunion-parcnational':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnre_style.css.less');
+			break;
+		case 'vanoise-parcnational':
+			drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
+			break;
+		default :
+			$google_validation_key = 'none';
   }
 }
 // AR staging instance :
@@ -166,4 +171,21 @@ else {
 	  drupal_add_css(drupal_get_path('theme', 'pnf') . '/less/pnva_style.css.less');
 	  break;
   }
+}
+
+function pnf_preprocess_html(&$vars) {
+	global $google_validation_key;
+
+	if ($google_validation_key != 'none') {
+		$google_webmasters_verification = array(
+			'#type' => 'html_tag',
+			'#tag' => 'meta',
+			'#attributes' => array(
+				'name' => 'google-site-verification',
+				'content' => $google_validation_key,
+			)
+		);
+
+		drupal_add_html_head($google_webmasters_verification, 'google_webmasters_verification');
+	}
 }
