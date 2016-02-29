155c8b5c22432665557cd382d85aa217acd390a1
Christophe Espiau <christophe.espiau@korora.fr>
2016-02-29 19:38:19 +0100

    PNF custom theme up: Close #195: Scald file template customized to remove the PDF icon displayed just before file name.

---
+++ repository/themes/custom/pnf/templates/scald-file-render.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ *   Default theme implementation for Scald File Render.
+ */
+?>
+<a href="<?php print file_create_url($vars['file_source']); ?>" title="<?php print $vars['file_title']; ?>">
+  <?php print $vars['file_title']; ?>
+</a>
