627924cc60c865f9f3bc3ee8c3fc0d1ffb824d0d
Christophe Espiau <christophe.espiau@korora.fr>
2016-04-08 14:29:41 +0200

    PNF custom theme up: PNV specific ckeditor settings added.

--- repository/themes/custom/pnf/js/ckeditor.styles.js
+++ repository/themes/custom/pnf/js/ckeditor.styles.js
@@ -75,6 +75,41 @@ if (typeof(CKEDITOR) !== 'undefined') {
       ]
     );
   }
+  else if (parkCode == 'pnv') {
+    CKEDITOR.addStylesSet('drupal',
+      [
+        /* Block Styles */
+        // These styles are already available in the "Format" drop-down list, so they are
+        // not needed here by default. You may enable them to avoid placing the
+        // "Format" drop-down list in the toolbar, maintaining the same features.
+        {name: 'Paragraphe', element: 'p'},
+        {name : 'Chapeau' , element: 'div', attributes: {'class': 'intro'}, styles: {'color': '#001489','font-style': 'italic' }},
+        {name: 'Bouton', element: 'div', attributes: {'class': 'boutonok'}, styles: {'color': '#fff'}},
+        {name: 'Titre 3', element: 'h3'},
+        {name: 'Titre 4', element: 'h4'},
+        {name: 'Titre 5', element: 'h5'},
+        {name: 'Paragraphe bleu foncé', element: 'p', styles: {'color': '#001489'}},
+        {name: 'Paragraphe bleu clair', element: 'p', styles: {'color': '#99D6EA'}},
+        {name: 'Paragraphe vert', element: 'p', styles: {'color': '#78BE20'}},
+        {name: 'Paragraphe marron', element: 'p', styles: {'color': '#b86125'}},
+        {name: 'Titre 3 - bleu', element: 'h3', styles: {'color': '#001489'}},
+        {name: 'Titre 3 - vert', element: 'h3', styles: {'color': '#78BE20'}},
+        {name: 'Titre 3 - marron', element: 'h3', styles: {'color': '#A1561C'}},
+
+        /* Object Styles */
+        {
+          name: 'Image on Left',
+          element: 'img',
+          attributes: {'style': 'padding: 5px; margin-right: 5px', 'border': '2', 'align': 'left'}
+        },
+        {
+          name: 'Image on Right',
+          element: 'img',
+          attributes: {'style': 'padding: 5px; margin-left: 5px', 'border': '2', 'align': 'right'}
+        }
+      ]
+    );
+  }
   else {
     CKEDITOR.addStylesSet('drupal',
       [
