a558dd29f1227afc854581bba5684a408969d782
Christophe Espiau <christophe.espiau@korora.fr>
2016-05-06 17:12:52 +0200

    Custom theme up: Missing ckeditor color codes added for PNP.

--- repository/themes/custom/pnf/js/ckeditor.styles.js
+++ repository/themes/custom/pnf/js/ckeditor.styles.js
@@ -215,6 +215,40 @@ if (typeof(CKEDITOR) !== 'undefined') {
       ]
     );
   }
+  else if (parkCode == 'pnp') {
+    CKEDITOR.addStylesSet('drupal',
+      [
+        /* Block Styles */
+        // These styles are already available in the "Format" drop-down list, so they are
+        // not needed here by default. You may enable them to avoid placing the
+        // "Format" drop-down list in the toolbar, maintaining the same features.
+        {name: 'Paragraphe', element: 'p'},
+        {name : 'Chapeau' , element: 'div', attributes: {'class': 'intro'}, styles: {'color': '#00965e','font-style': 'italic' }},
+        {name: 'Bouton', element: 'div', attributes: {'class': 'boutonok'}, styles: {'color': '#fff'}},
+        {name: 'Titre 3', element: 'h3'},
+        {name: 'Titre 4', element: 'h4'},
+        {name: 'Titre 5', element: 'h5'},
+        {name: 'Paragraphe vert foncé', element: 'p', styles: {'color': '#00965e'}},
+        {name: 'Paragraphe vert clair', element: 'p', styles: {'color': '#78be20'}},
+        {name: 'Paragraphe bleu', element: 'p', styles: {'color': '#003da5'}},
+        {name: 'Titre 3 - vert foncé', element: 'h3', styles: {'color': '#00965e'}},
+        {name: 'Titre 3 - vert', element: 'h3', styles: {'color': '#78be20'}},
+        {name: 'Titre 3 - bleu', element: 'h3', styles: {'color': '#003da5'}},
+
+        /* Object Styles */
+        {
+          name: 'Image on Left',
+          element: 'img',
+          attributes: {'style': 'padding: 5px; margin-right: 5px', 'border': '2', 'align': 'left'}
+        },
+        {
+          name: 'Image on Right',
+          element: 'img',
+          attributes: {'style': 'padding: 5px; margin-left: 5px', 'border': '2', 'align': 'right'}
+        }
+      ]
+    );
+  }
   else {
     CKEDITOR.addStylesSet('drupal',
       [
