8a7991b94cc72355b2221f4615599a2f88d43d98
Christophe Espiau <christophe.espiau@gmail.com>
2016-10-13 14:42:50 +0200

    PNx profile up: Custom ckeditor styles altered for PNP.

--- repository/themes/custom/pnf/js/ckeditor.styles.js
+++ repository/themes/custom/pnf/js/ckeditor.styles.js
@@ -1,7 +1,6 @@
 /*
 For PNF
  */
-
 var parkCode = Drupal.settings.kcore.park_code;
 
 if (typeof(CKEDITOR) !== 'undefined') {
@@ -222,18 +221,51 @@ if (typeof(CKEDITOR) !== 'undefined') {
         // These styles are already available in the "Format" drop-down list, so they are
         // not needed here by default. You may enable them to avoid placing the
         // "Format" drop-down list in the toolbar, maintaining the same features.
-        {name: 'Paragraphe', element: 'p'},
-        {name : 'Chapeau' , element: 'div', attributes: {'class': 'intro'}, styles: {'color': '#00965e','font-style': 'italic' }},
+        {name: 'Paragraphe', element: 'p', styles: {
+          'text-align': 'justify'
+        }},
+        {name : 'Chapeau' , element: 'div', attributes: {'class': 'intro'}, styles: {
+          'color': '#758298',
+          'text-align': 'justify',
+          'font-style': 'normal',
+          'font-weight': 'bold',
+          'font-size': '16px'
+        }},
         {name: 'Bouton', element: 'div', attributes: {'class': 'boutonok'}, styles: {'color': '#fff'}},
-        {name: 'Titre 3', element: 'h3'},
-        {name: 'Titre 4', element: 'h4'},
-        {name: 'Titre 5', element: 'h5'},
+        //{name: 'Titre 3', element: 'h3'},
+        {name: 'Titre 3', element: 'h3', styles: {
+          'color': '#00965e',
+          'font-weight': 'normal',
+          'font-size': '24px',
+          'margin-top': '20px',
+          'margin-bottom': '15px'
+        }},
+        {name: 'Titre 4 vert', element: 'h4', styles: {
+          'color': '#00965e',
+          'font-weight': 'bold',
+          'font-size': '14px',
+          'margin-top': '10px',
+          'margin-bottom': '10px'
+        }},
+        {name: 'Titre 4', element: 'h4', styles: {
+          'color': '#000',
+          'font-weight': 'bold',
+          'font-size': '14px',
+          'margin-top': '10px',
+          'margin-bottom': '10px'
+
+        }},
+        {name: 'Titre 5', element: 'h5', styles: {
+          'color': '#000',
+          'font-weight': 'bold',
+          'font-size': '14px',
+          'margin-top': '5px',
+          'margin-bottom': '5px'
+
+        }},
         {name: 'Paragraphe vert foncé', element: 'p', styles: {'color': '#00965e'}},
         {name: 'Paragraphe vert clair', element: 'p', styles: {'color': '#78be20'}},
         {name: 'Paragraphe bleu', element: 'p', styles: {'color': '#003da5'}},
-        {name: 'Titre 3 - vert foncé', element: 'h3', styles: {'color': '#00965e'}},
-        {name: 'Titre 3 - vert', element: 'h3', styles: {'color': '#78be20'}},
-        {name: 'Titre 3 - bleu', element: 'h3', styles: {'color': '#003da5'}},
 
         /* Object Styles */
         {
