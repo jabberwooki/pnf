549b83980466dab3198512868ed7ed641ec6dea6
Christophe Espiau <christophe.espiau@gmail.com>
2017-03-29 11:53:32 +0200

    pnf_newletter and pnf_mailchimp up: Reorganisation of ccs and js files.

--- repository/modules/features/pnf_mailchimp/js/pnf_mailchimp.js
+++ repository/modules/features/pnf_mailchimp/js/pnf_mailchimp.js
@@ -5,7 +5,8 @@
 (function ($) {
   Drupal.behaviors.mailchimp = {
     attach: function (context, settings) {
-
+      var infoText = '<p class="mailchimp-signup-info">Inscrivez-vous et recevez régulièrement<br/>nos dernières actualités</p>';
+      $('#mailchimp-signup-subscribe-block-newsletter-block-form').append(infoText);
     }
   };
 }(jQuery));
