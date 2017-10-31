bbc2e7830de4ef213785ec2f2d9176e6155b83fa
Christophe Espiau <christophe.espiau@gmail.com>
2017-10-31 11:23:09 +0100

    pnf_scientific_reports up: Date and Title fields added as filter criteria in view scientific_reports/sr_list.

--- repository/modules/features/pnf_scientific_reports/js/pnf_scientific_reports.js
+++ repository/modules/features/pnf_scientific_reports/js/pnf_scientific_reports.js
@@ -6,6 +6,11 @@
 (function ($) {
   Drupal.behaviors.scientificReports = {
     attach: function (context, settings) {
+      /* Teaser mode */
+      $('#edit-field-displayed-pub-date-value-value-date').attr('size', '3');
+      $('#edit-field-displayed-pub-date-value-value-date').attr('maxlength', '4');
+
+      /* Full mode */
       // alert($('.breadcrumb .last').text());
       var nodeTitle = $('.breadcrumb .last').text();
       // alert(nodeTitle.length);
