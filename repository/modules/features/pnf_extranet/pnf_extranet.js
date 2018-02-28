ae3afa9691ee5abb6d29ea30b8cb4132b8dc262f
Christophe Espiau <christophe.espiau@gmail.com>
2018-02-28 10:46:49 +0100

    pnf_extranet completed.

--- repository/modules/features/pnf_extranet/pnf_extranet.js
+++ repository/modules/features/pnf_extranet/pnf_extranet.js
@@ -5,40 +5,53 @@
 (function ($) {
   Drupal.behaviors.extranet = {
     attach: function (context, settings) {
-      $('select#edit-parent').removeAttr('multiple');
+      // Taxonomy term add/edit form handling
+      if ($('select#edit-parent').length) {
+        $('select#edit-parent').removeAttr('multiple');
 
-      var termPerRoleSettings = settings.extranet.termPerRoleSettings;
-      var extranetManagerRid = settings.extranet.extranetManagerRid;
-      var previouslySelectedOption = 0;
+        var termPerRoleSettings = settings.extranet.termPerRoleSettings;
+        var extranetManagerRid = settings.extranet.extranetManagerRid;
+        var previouslySelectedOption = 0;
 
-      $('select#edit-parent').change(function () {
-        selectedOption = $('select#edit-parent option:selected').val();
+        $('select#edit-parent').change(function () {
+          selectedOption = $('select#edit-parent option:selected').val();
 
-        if(selectedOption > 0) {
-          termPerRoleSettings[selectedOption].rids.forEach(function (rid) {
-            if (rid != extranetManagerRid) {
-              $('#edit-term-per-role-roles-' + rid).prop('checked', true);
+          if (selectedOption > 0) {
+            termPerRoleSettings[selectedOption].rids.forEach(function (rid) {
+              if (rid != extranetManagerRid) {
+                $('#edit-term-per-role-roles-' + rid).prop('checked', true);
+              }
+            });
+
+            if (previouslySelectedOption > 0) {
+              termPerRoleSettings[previouslySelectedOption].rids.forEach(function (rid) {
+                if (rid != extranetManagerRid) {
+                  $('#edit-term-per-role-roles-' + rid).prop('checked', false);
+                }
+              });
             }
-          });
-
-          if (previouslySelectedOption > 0) {
+            previouslySelectedOption = selectedOption;
+          }
+          else {
             termPerRoleSettings[previouslySelectedOption].rids.forEach(function (rid) {
               if (rid != extranetManagerRid) {
                 $('#edit-term-per-role-roles-' + rid).prop('checked', false);
               }
             });
+            previouslySelectedOption = selectedOption;
           }
-          previouslySelectedOption = selectedOption;
-        }
-        else {
-          termPerRoleSettings[previouslySelectedOption].rids.forEach(function (rid) {
-            if (rid != extranetManagerRid) {
-              $('#edit-term-per-role-roles-' + rid).prop('checked', false);
-            }
-          });
-          previouslySelectedOption = selectedOption;
-        }
-      });
+        });
+      }
+
+      // Extranet views handling
+      if ($('#block-pnf-extranet-extranet-folders')) {
+        $('#block-pnf-extranet-extranet-folders').addClass('col-md-4');
+        $('#block-views-extranet-folders-root-folders').addClass('col-md-8');
+        $('#block-views-extranet-folders-child-folders').addClass('col-md-8');
+        $('#block-system-main').addClass('col-md-7');
+
+        // $('.region-content section').matchHeight();
+      }
     }
   };
 }(jQuery));
